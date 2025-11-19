1ea17f349c6233f23da94211f75f8c5ccdee5df9
uo285395 <uo285395@uniovi.es>
2025-11-19 12:33:08 +0100

    API_URL production

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000";
+// const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://backend-disponibilidad-production.up.railway.app.onrender.app"  
 
 // ------------------- TOKEN STORAGE -------------------
 
